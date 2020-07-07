029bbdf5c880b76b7833bc16479d8252e3377bbd
jdallapola <j.dallapola@gmail.com>
2020-07-07 19:56:57 -0300

    A script to plot the frequency of a user-defined term throughout CIEX documents.

---
+++ Term_Graph_Plot.R
@@ -0,0 +1,58 @@
+# Cleaning Work Environment #
+
+rm(list = ls())
+
+# Calling Required Elements #
+
+# Years List
+
+
+# Sourcing and Running Text Mining Script
+source("https://raw.githubusercontent.com/jdallapola/ciex/master/ciex_PDF_mining.R")
+
+# Front End
+######################################################################################################
+search_key <- "uruguai" #insert search key here
+go_search() # initiate search mechanism and graph plotter
+
+# Under the hood
+######################################################################################################
+go_search <- function()
+  
+{
+  
+  years = read.csv("https://raw.githubusercontent.com/jdallapola/ciex/master/years_CIEX.csv")
+  
+  search_directory = as.matrix(ciex_pdfs.dtm)%>%
+    data.frame()%>%
+    select(search_key)%>%
+    data.frame()
+  
+  search_directory$years = years
+  search_directory = setNames(search_directory,c("Freq.","Years"))
+  search_directory = setNames(aggregate(search_directory$Freq., by = search_directory$Years, FUN = sum), c("Years","Freq."))
+  
+  # Graph elements #
+  n = sum(search_directory$Freq.)  
+  title = paste("Frequency of the word"," '",search_key,"' ","in CIEX Documents", sep = " ", collapse = NULL)
+  subtitle = paste("n = ",n,"
+                     ", sep = "", collapse = NULL)
+  windowsFonts(A = windowsFont("Louis George Café"))
+  
+  # Structuring Graph #  
+  
+  p <- ggplot(search_directory, aes(x=Years, y=Freq., group=1))+
+    geom_line(color="#6891C3", size = 1.2) +
+    ggtitle(title,subtitle=subtitle) +
+    xlab("
+      Year
+      ") +
+    ylab("Frequency
+           ") + 
+    theme(text=element_text(size = 25,
+                            family = "A"))
+  
+  p # Plotting Graph
+  
+  
+}
