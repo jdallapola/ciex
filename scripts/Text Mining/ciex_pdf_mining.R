e2e0bd9de8f3a0c3dd37a2d4231a71e25361bb5c
jdallapola <j.dallapola@gmail.com>
2020-07-21 14:55:24 -0300

    Integration updates

--- scripts/Text Mining/ciex_pdf_mining.R
+++ scripts/Text Mining/ciex_pdf_mining.R
@@ -9,7 +9,7 @@ library(stringr)
 library(plyr)
 library(data.table)
 library(ggplot2)
-rm(list = ls())
+
 
 # Following instructions at https://uvastatlab.github.io/2019/05/14/reading-pdf-files-into-r-for-text-mining/ #
 
@@ -45,4 +45,43 @@ corp <- VCorpus(VectorSource(ciex_pdfs))
                                                tolower = TRUE,
                                                removeNumbers = TRUE,
                                                bounds = list(global = c(3, Inf)))) 
+    # Search and graph plotter tool
+    
+    # Importing PDF Years List
+    
+    years <- read.csv("https://raw.githubusercontent.com/jdallapola/ciex/master/data/years_CIEX.csv")
+    
+    go_search <- function(){
+      
+      search_directory = as.matrix(ciex_pdfs.dtm)%>%
+        data.frame()%>%
+        select(tolower(search_key))%>%
+        cbind(years)%>%
+        setNames(c("Freq.","Years"))
+      
+      search_directory = setNames(aggregate(search_directory$Freq.,
+                                            by = list(Years = search_directory$Years), 
+                                            FUN = sum), 
+                                  c("Years","Freq."))
+      
+      # Graph elements #
+      n <- sum(search_directory$Freq.)
+      title <- paste0("Frequency of the word ","''",search_key,"''"," in CIEX Documents", collapse = NULL)
+      subtitle <- paste("n = ",n, collapse = NULL)
+      windowsFonts(A = windowsFont("Louis George Caf?"))
+      
+      # Structuring and Plotting Graph #  
+      
+      ggplot(search_directory, aes(x=Years, y=Freq., group=1))+
+        geom_line(color="#6891C3", size = 1.2) +
+        ggtitle(title,subtitle=subtitle) +
+        xlab("Year") +
+        ylab("Frequency") + 
+        theme(text = element_text(size = 15,family = "A"),
+              axis.title.x = element_text(margin = margin(t=20,r=0,b=20,l=0)),
+              axis.title.y = element_text(margin = margin(t=00,r=20,b=0,l=20)),
+              plot.subtitle = element_text(size = 10, margin = margin(t=0,r=0,b=20,l=0)))
+    }   
+    
+    
     
--- scripts/Text Mining/graph_terms_plot.R
+++ scripts/Text Mining/graph_terms_plot.R
@@ -1,10 +1,5 @@
 
-# Calling Required Elements #
-
-    # Importing PDF Years List
-    years <- read.csv("https://raw.githubusercontent.com/jdallapola/ciex/master/data/years_CIEX.csv")
-    
-    # Sourcing and Running Text Mining Script (WARNING: Will take a few seconds to run)
+# Sourcing and Running Text Mining Script (WARNING: Will take a few seconds to run)
     source("https://raw.githubusercontent.com/jdallapola/ciex/master/scripts/Text%20Mining/ciex_pdf_mining.R")        
                                                                                                   
 # --------------------------------------------------------------------------------------------------    
@@ -13,43 +8,3 @@
 
 search_key <- "Portugal" #insert search key here
 go_search() # initiate search mechanism and graph plotter
-
-head(search_directory)
-# ---------------------------------------------
-## Under the hood
-
-go_search <- function()
-  
-{
-  
-  search_directory = as.matrix(ciex_pdfs.dtm)%>%
-    data.frame()%>%
-    select(tolower(search_key))%>%
-    cbind(years)%>%
-    setNames(c("Freq.","Years"))
-  
-    search_directory = setNames(aggregate(search_directory$Freq.,
-                                          by = list(Years = search_directory$Years), 
-                                          FUN = sum), 
-                              c("Years","Freq."))
-    
-   # Graph elements #
-  n <- sum(search_directory$Freq.)
-  title <- paste0("Frequency of the word ","''",search_key,"''"," in CIEX Documents", collapse = NULL)
-  subtitle <- paste("n = ",n, collapse = NULL)
-  windowsFonts(A = windowsFont("Louis George Caf?"))
-  
-  # Structuring and Plotting Graph #  
-  
-  ggplot(search_directory, aes(x=Years, y=Freq., group=1))+
-    geom_line(color="#6891C3", size = 1.2) +
-    ggtitle(title,subtitle=subtitle) +
-    xlab("Year") +
-    ylab("Frequency") + 
-    theme(text = element_text(size = 15,family = "A"),
-          axis.title.x = element_text(margin = margin(t=20,r=0,b=20,l=0)),
-          axis.title.y = element_text(margin = margin(t=00,r=20,b=0,l=20)),
-          plot.subtitle = element_text(size = 10, margin = margin(t=0,r=0,b=20,l=0)))
-}  
-    
-    
